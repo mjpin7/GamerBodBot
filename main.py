6cf31d1aa51cc0da5d3cc065178275b945a5174e
MJ Pinheiro <mjpin7@gmail.com>
2019-04-14 13:46:57 -0400

    fixed json error

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def function_meme(self, message, client, args):
     try:
         # Refresh token
         resp = requests.get("https://gamerbodbot-api.herokuapp.com/refresh", headers={"Authorization": "Bearer " + os.environ.get('JWT_TOKEN')})
-        os.environ["JWT_TOKEN"] = resp["access_token"]
+        os.environ["JWT_TOKEN"] = resp.json()["access_token"]
 
         # Make request for the meme
         resp = requests.get("https://gamerbodbot-api.herokuapp.com/meme", headers={"Authorization": "Bearer " + os.environ.get('JWT_TOKEN')})
