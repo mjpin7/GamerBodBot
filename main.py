596edf3c8fc60d7c371b4bcebc352b5d78a231c6
MJ <mjpin7@gmail.com>
2019-05-03 19:58:59 -0400

    Redid the backlog command for effeciency

--- main.py
+++ main.py
@@ -74,6 +74,56 @@ handler.add_command({
     'desc': 'Sends greetings to the user'
 })
 
+# Function to handle the commands command
+#
+# Returns a list of the commands
+def function_commands(self, message, client, args):
+    try:
+        response = "Commands:\n```"
+        
+        for command in self.commands:
+            response += command['trigger']
+
+            # Add the args (if any) to the response
+            if command['trigger'] == "!backlog":
+                response += " {} <{}>\n".format(command['args_val'][0], command['args_val'][1])
+                response += "{} <{}>".format(command['trigger'], command['args_val'][1])
+            else:
+                for arg in command['args_val']:
+                    response += " <{}>".format(arg)
+            
+
+            response += ": {}\n\n".format(command['desc'])
+
+        response+= "```"
+
+        return response
+    except Exception as e:
+        return e
+    
+handler.add_command({
+    'trigger': '!commands',
+    'function': function_commands,
+    'number_args': 0,
+    'args_val': [],
+    'desc': 'Lists commands'
+})
+
+# Function to handle the help command
+def function_help(self, message, client, args):
+    try:
+        return "Type in command as ```!<command>``` or type in ```!commands``` for list of commands"
+    except  Exception as e:
+        return e
+
+handler.add_command({
+    'trigger': '!help',
+    'function': function_help,
+    'number_args': 0,
+    'args_val': [],
+    'desc': 'Helps user'
+})
+
 # Function to handle the meme command
 #
 # References the google custom search api and a created custom google search to search multiple sites for a meme. Returns a random image.
@@ -105,54 +155,18 @@ handler.add_command({
     'desc': 'Sends a meme to user'
 })
 
-# Function to handle the commands command
-#
-# Returns a list of the commands
-def function_commands(self, message, client, args):
+def function_backlog(self, message, client, args):
     try:
-        response = "Commands:\n```"
-        
-        for command in self.commands:
-            response += command['trigger']
-
-            # Add the args (if any) to the response
-            for arg in command['args_val']:
-                response += " <{}>".format(arg)
-
-            response += ": {}\n".format(command['desc'])
-
-        response+= "```"
-
-        return response
-    except Exception as e:
-        return e
-    
-handler.add_command({
-    'trigger': '!commands',
-    'function': function_commands,
-    'number_args': 0,
-    'args_val': [],
-    'desc': 'Lists commands'
-})
-
-# Function to handle the help command
-def function_help(self, message, client, args):
-    try:
-        return "Type in command as ```!<command>``` or type in ```!commands``` for list of commands"
-    except  Exception as e:
-        return e
-
-handler.add_command({
-    'trigger': '!help',
-    'function': function_help,
-    'number_args': 0,
-    'args_val': [],
-    'desc': 'Helps user'
-})
-
-def function_backlog_add(self, message, client, args):
-    try:
-        resp = requests.post("https://gamerbodbot-api.herokuapp.com/backlog/{}".format(message.author.display_name), data={"game": "{}".format(args[0]), "status": "unplayed"}, headers={"Authorization": "Bearer " + os.environ.get('JWT_TOKEN')})
+        if args[0] == "add":
+            resp = requests.post("https://gamerbodbot-api.herokuapp.com/backlog/{}".format(message.author.display_name), data={"game": "{}".format(args[1]), "status": "unplayed"}, headers={"Authorization": "Bearer " + os.environ.get('JWT_TOKEN')})
+        elif args[0] == "finished":
+            resp = requests.put("https://gamerbodbot-api.herokuapp.com/backlog/{}".format(message.author.display_name), data={"game": "{}".format(args[1]), "status": "finished"}, headers={"Authorization": "Bearer " + os.environ.get('JWT_TOKEN')})
+        elif args[0] == "playing":
+            resp = requests.put("https://gamerbodbot-api.herokuapp.com/backlog/{}".format(message.author.display_name), data={"game": "{}".format(args[1]), "status": "playing"}, headers={"Authorization": "Bearer " + os.environ.get('JWT_TOKEN')})
+        elif args[0] == "all":
+            pass
+        else:
+            resp = requests.get("https://gamerbodbot-api.herokuapp.com/backlog/{}".format(message.author.display_name), data={"game": "{}".format(args[0])}, headers={"Authorization": "Bearer " + os.environ.get('JWT_TOKEN')})
 
         if resp.status_code != 200:
             if resp.status_code == 401:
@@ -168,61 +182,11 @@ def function_backlog_add(self, message, client, args):
 
 
 handler.add_command({
-    'trigger': '!backlogadd',
-    'function': function_backlog_add,
+    'trigger': '!backlog',
+    'function': function_backlog,
     'number_args': 1,
-    'args_val': ['game'],
-    'desc': 'Adds a backlog item to the users backlog'
-})
-
-def function_playing(self, message, client, args):
-    try:
-        resp = requests.put("https://gamerbodbot-api.herokuapp.com/backlog/{}".format(message.author.display_name), data={"game": "{}".format(args[0]), "status": "playing"}, headers={"Authorization": "Bearer " + os.environ.get('JWT_TOKEN')})
-
-        if resp.status_code != 200:
-            if resp.status_code == 401:
-                raise ApiError('Get error {}, unauthorized. Message contents: ```javascript\n{}\n```'.format(resp.status_code, resp.json()))
-            else:
-                raise ApiError('Get error {}. Message contents: ```javascript\n{}\n```'.format(resp.status_code, resp.json()))
-        
-        return resp.json()['message']
-    except ApiError as a:
-        return a
-    except Exception as e:
-        return e
-
-
-handler.add_command({
-    'trigger': '!playing',
-    'function': function_playing,
-    'number_args': 1,
-    'args_val': ['game'],
-    'desc': 'Changes a backlog item passed in to status: playing'
-})
-
-def function_finished(self, message, client, args):
-    try:
-        resp = requests.put("https://gamerbodbot-api.herokuapp.com/backlog/{}".format(message.author.display_name), data={"game": "{}".format(args[0]), "status": "finished"}, headers={"Authorization": "Bearer " + os.environ.get('JWT_TOKEN')})
-
-        if resp.status_code != 200:
-            if resp.status_code == 401:
-                raise ApiError('Get error {}, unauthorized. Message contents: ```javascript\n{}\n```'.format(resp.status_code, resp.json()))
-            else:
-                raise ApiError('Get error {}. Message contents: ```javascript\n{}\n```'.format(resp.status_code, resp.json()))
-        
-        return resp.json()['message']
-    except ApiError as a:
-        return a
-    except Exception as e:
-        return e
-
-
-handler.add_command({
-    'trigger': '!finished',
-    'function': function_finished,
-    'number_args': 1,
-    'args_val': ['game'],
-    'desc': 'Changes a backlog item passed in to status: finished'
+    'args_val': ['add/finished/playing', 'game'],
+    'desc': 'Command to interact (add, finsih, update) backlog items'
 })
 
 @client.event  # event decorator/wrapper (anytime some event is going to occur)
